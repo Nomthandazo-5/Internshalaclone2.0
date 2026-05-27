19111eb9d9fbc537a8be4078b0a4164c558ed418
Nomthandazo <ngwenyanomthandazo47@gmail.com>
2026-05-27 16:16:13 +0530

    Updated files in my folder

--- internarea/next.config.ts
+++ internarea/next.config.ts
@@ -12,11 +12,12 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              connect-src 'self' https://internshalaclone2-0-1.onrender.com;
+              connect-src 'self' https://internshalaclone2-0-1.onrender.com https://*.googleapis.com https://*.firebaseapp.com https://*.gstatic.com;
               img-src 'self' data: blob: https:;
               font-src 'self' https: data:;
               style-src 'self' 'unsafe-inline';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://*.gstatic.com;
+              frame-src 'self' https://accounts.google.com https://*.firebaseapp.com;
             `
               .replace(/\n/g, " ")
               .trim(),
